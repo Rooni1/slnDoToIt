d99896f609abd6285ecc15e747077872f25ffc6e
Haroon <haroon_munir@hotmail.com>
2021-06-28 13:23:28 +0200

    Add project files.

---
+++ DoToIt/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DoToIt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
