bfc9071e10cc80c9aee080651d41f52133ad9859
Billy Rösehag <billyrosehag@gmail.com>
2021-06-29 11:07:37 +0200

    Have implemented the Todo class and the testning for that class

---
+++ DoToIt/Model/Todo.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ToDoIt.Model
+{
+    public class Todo
+    {
+        //Private Fields
+        private readonly int todoId;// Can only be set once, then the object will not be able to change its unique todoId;
+        private string description;
+        private bool done;
+        Person assignee;
+
+        //Public Properties
+        public int ToDoId { get { return todoId; } }
+        public bool Done { get { return done; } }
+        public string Description
+        {
+            get { return description; }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    Console.WriteLine("Description cannot be empty. Please fill in a description.");
+                }
+                else
+                {
+                    description = value;
+                }
+
+            }
+        }
+
+        //Constructor
+        public Todo(string description, int todoId)
+        {
+            this.Description = description;
+            this.todoId = todoId;
+        }
+
+    }
+}
