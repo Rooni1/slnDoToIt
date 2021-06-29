a5fd39a30325dd9ce07204a689e73b1cdd943cde
Haroon <haroon_munir@hotmail.com>
2021-06-29 12:06:41 +0200

    Added New classes

---
+++ DoToIt/Data/PersonSequencer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ToDoIt.Data
+{
+    public class PersonSequencer
+    {
+        private static int personId;
+        public int PersonId { get { return personId; } }
+
+        public static int nextPersonId()
+        {
+            return ++personId;
+        }
+        public static void Reset()
+        {
+            personId = 0;
+        }
+        
+    }
+}
